c50091963f238b76091c8dfedc4f5f19d6b4baa3
Brendon Waters <brendonwaters@uvic.ca>
2023-03-05 16:29:20 -0800

    Added BigO analysis

--- ArrayMatch.java
+++ ArrayMatch.java
@@ -6,6 +6,16 @@
 
 /* 
 Algorithm analysis goes here.
+For the base case the T(n) is constant because if the size of the int arrays are both 1.
+This is not the worst case scenario and so does not affect
+the worst case time complexity.
+So, this algorithm divides 2 arrays each into a total of 4 sub-arrays
+and does so recursively k times which results in a BigO of 2^k in the worst case
+for k being the size of the arrays.
+Therefore T(n) = c_1n + T(n/2) + T(n/2) + c_2n for
+constant time divides, 2 recursive calls on each array and contant time comparisons respectively
+which in the worst case means that n = 2^k for k => 1 which simplifies to T(n) = nlogn
+when converted from open form to closed form.
 */
 
 import java.io.*;
@@ -24,34 +34,34 @@ public class ArrayMatch {
 		/* Recursive Case */
 		if (a.length % 2 == 0 && b.length % 2 == 0) {
 			int n = a.length;
-			int[] a1 = new int[n/2];
-			int[] a2 = new int[n/2];
-			int[] b1 = new int[n/2];
-			int[] b2 = new int[n/2];
+			int[] a1 = new int[n / 2];
+			int[] a2 = new int[n / 2];
+			int[] b1 = new int[n / 2];
+			int[] b2 = new int[n / 2];
 			int i, j;
-			for (i = 0; i < n/2; i++) {
+			for (i = 0; i < n / 2; i++) {
 				a1[i] = a[i];
 				b1[i] = b[i];
 			}
-			for (j = 0; j < n/2; j++) {
+			for (j = 0; j < n / 2; j++) {
 				a2[j] = a[i];
 				b2[j] = b[i];
 				i++;
 			}
-			return (match(a1, b1) && match(a2, b2) || match(a1, b1) && match(a1, b2) || match(a2, b1) && match (a2, b2));
+			return (match(a1, b1) && match(a2, b2) || match(a1, b1) && match(a1, b2) || match(a2, b1) && match(a2, b2));
 		}
 		if (a.length % 2 == 1 && b.length % 2 == 1) {
 			int n = a.length;
-			int[] a1 = new int[n/2 + 1];
-			int[] a2 = new int[n/2];
-			int[] b1 = new int[n/2 + 1];
-			int[] b2 = new int[n/2];
+			int[] a1 = new int[n / 2 + 1];
+			int[] a2 = new int[n / 2];
+			int[] b1 = new int[n / 2 + 1];
+			int[] b2 = new int[n / 2];
 			int i, j;
-			for (i = 0; i < n/2 + 1; i++) {
+			for (i = 0; i < n / 2 + 1; i++) {
 				a1[i] = a[i];
 				b1[i] = b[i];
 			}
-			for (j = 0; j < n/2; j++) {
+			for (j = 0; j < n / 2; j++) {
 				a2[j] = a[i];
 				b2[j] = b[i];
 				i++;
@@ -83,13 +93,13 @@ public class ArrayMatch {
 			try {
 				s = new Scanner(new File(args[0]));
 			} catch (java.io.FileNotFoundException e) {
-				// System.out.printf("Unable to open %s\n", args[0]);
+				System.out.printf("Unable to open %s\n", args[0]);
 				return;
 			}
-			// System.out.printf("Reading input values from %s.\n", args[0]);
+			System.out.printf("Reading input values from %s.\n", args[0]);
 		} else {
 			s = new Scanner(System.in);
-			// System.out.printf("Reading input values from stdin.\n");
+			System.out.printf("Reading input values from stdin.\n");
 		}
 
 		int n = s.nextInt();
@@ -103,7 +113,6 @@ public class ArrayMatch {
 		for (int j = 0; j < n; j++) {
 			b[j] = s.nextInt();
 		}
-
 		System.out.println((match(a, b) ? "YES" : "NO"));
 	}
 }
